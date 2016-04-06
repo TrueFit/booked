397dcf3060a07bb39e2480835cdd6365bd4df48f
jgretz <jgretz@truefitsolutions.com>
2016-04-06 19:36:52 -0400

    update to be a little more nicely formatted and support passing in the token from the environment variables

--- api/src/routes/events.js
+++ api/src/routes/events.js
@@ -4,13 +4,28 @@ const _ = require('underscore');
 const outlook = require("node-outlook");
 const tokenCache = require("../services/token_cache.js");
 
+const getAccessToken = () => {
+  const token = tokenCache.getToken();
+  if (token) {
+    return token.token.access_token;
+  }
+
+  return process.env.ACCESS_TOKEN;
+};
+
 module.exports = {
   get: (req, res) => {
-    outlook.base.setApiEndpoint('https://outlook.office.com/api/v2.0');
+    // check for access token
+    const accessToken = getAccessToken();
+    if (!accessToken) {
+      res.sendStatus(500);
+      return;
+    }
 
-    const accessToken = tokenCache.getToken().token.access_token;
+    // configure end point
+    outlook.base.setApiEndpoint('https://outlook.office.com/api/v2.0');
     const calendarsParams = {
-      url: 'https://outlook.office.com/api/v2.0/me/calendars',
+      url: 'https://outlook.office.com/api/v2.0/users/carver@truefit.io/calendars',
       token: accessToken
     };
 
@@ -20,29 +35,34 @@ module.exports = {
         return;
       }
 
-      const calendar = _.find(result.body.value, c => c.Name == "Calendar");
-      if (!calendar) {
-        res.json("sad panda");
-        return;
-      }
 
-      const eventParams = {
-        token: accessToken,
-        useMe: true,
-        calendarId: calendar.Id
-      };
 
-      outlook.calendar.getEvents(eventParams, (err, result) => {
-        if (err) {
-          next(err);
-          return;
-        }
+      console.log(result);
+      res.json({msg:"hi"});
 
-        console.log(result.value);
-
-        res.json('hi');
-
-      });
+      // const calendar = _.find(result.body.value, c => c.Name == "Calendar");
+      // if (!calendar) {
+      //   res.json("sad panda");
+      //   return;
+      // }
+      //
+      // const eventParams = {
+      //   token: accessToken,
+      //   useMe: true,
+      //   calendarId: calendar.Id
+      // };
+      //
+      // outlook.calendar.getEvents(eventParams, (err, result) => {
+      //   if (err) {
+      //     next(err);
+      //     return;
+      //   }
+      //
+      //   console.log(result.value);
+      //
+      //   res.json({msg:'hi'});
+      //
+      // });
     });
   }
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -5,15 +5,28 @@ const tokenCache = require('../services/token_cache.js');
 
 module.exports = {
   get: (req, res) => {
-    const tokenExists = tokenCache.getToken() ? true : false;
+    const token = tokenCache.getToken();
 
-    if (tokenExists) {
+    if (token) {
       const model = {
-        email: tokenCache.getEmail()
+        email: tokenCache.getEmail(),
+        token: token.token.access_token
       };
 
       res.render('index_token_exists', model);
+      return;
     }
+
+    const access_token = process.env.ACCESS_TOKEN;
+    if (access_token) {
+      const model = {
+        token: access_token
+      };
+
+      res.render('index_access_token_specified', model);
+      return;
+    }
+
     else {
       const model = {
         url: auth.generate_auth_url()
